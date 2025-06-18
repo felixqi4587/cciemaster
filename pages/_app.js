786f4fd57fd983f1325e4b2d4b03bfde600fe799
CCIE-Master <ccie@example.com>
2025-06-18 13:50:56 -0600

    集成stagewise工具栏：添加AI驱动的浏览器编辑功能

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css';
+import { StagewiseToolbar } from '@stagewise/toolbar-next';
+import { ReactPlugin } from '@stagewise-plugins/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <StagewiseToolbar 
+        config={{
+          plugins: [ReactPlugin()]
+        }}
+      />
+    </>
+  );
 }
 
 export default MyApp; 
