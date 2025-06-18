0f5ec9b8b800b974f3ae657005daf631e0e16048
CCIE-Master <ccie@example.com>
2025-06-18 13:53:09 -0600

    修复stagewise工具栏兼容性：使用基础包替代Next.js特定包

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,21 @@
 import '../styles/globals.css';
-import { StagewiseToolbar } from '@stagewise/toolbar-next';
-import { ReactPlugin } from '@stagewise-plugins/react';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <StagewiseToolbar 
-        config={{
-          plugins: [ReactPlugin()]
-        }}
-      />
-    </>
-  );
+  useEffect(() => {
+    // 只在客户端且开发模式下初始化stagewise工具栏
+    if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+      import('@stagewise/toolbar').then(({ initToolbar }) => {
+        import('@stagewise-plugins/react').then(({ ReactPlugin }) => {
+          initToolbar({
+            plugins: [ReactPlugin()],
+          });
+        });
+      });
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
 export default MyApp; 
